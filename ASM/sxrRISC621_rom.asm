19f326e5432d271a9f615f99a5a49195d72aff0b
Siddharth Ramkrishnan <sxr4316@rit.edu>
2016-11-10 20:53:58 -0500

    Assembly File to perform the necessary Vector Operation

--- ASM/sxrRISC621_rom.asm
+++ ASM/sxrRISC621_rom.asm
@@ -19,96 +19,46 @@
 ;------------------------------------------------------------------------------
 
 .code;
+			LD		RE, M[0+0x0001]; % Load the Case Change Offset %
 
-@Start			MULC	R0,		0	 ;
-				MULC	R1,		0	 ;
-				MUL		R0,		R1	 ;
-				CALL	@NegTestFail ;
-				NOT		R1;
-				ST		R1, M[0+0x0010];
-				NOT 	R1;
-				ADDC	R1,		5	 ;
-				CPY		R5,		R1	 ;
-				SWAP	R5,		RC	 ;	
-				LD		R9, M[0+0x0010];
-				JMP		@Start		 ;
+			LD		RF, M[0+0x0000]; % Load Loop Counter to 64 %
 
-@NegTestFail	SUB		R0,		R0	 ;
-				ADDC	R0,		1	 ;
-				JMPN	@Start		 ;
-@NegTestPass	SUB		R0,		R0	 ;
-				SUBC	R0,		1	 ;
-				JMPN	@OverTestFail;
+			SUB	R7, R7; % Reset Register R7 %
 
-@ZeroTestFail	SUB		R0,		R0	 ;
-				ADDC	R0,		1	 ;
-				JMPZ	@Start		 ;
-@ZeroTestPass	SUB		R0,		R0	 ;
-				JMPZ	@CarTestFail;
+@Start	LD		R0, M[R7+0x0010];
+			ST		R0, M[R7+0x0500];
+			ADDC	R7, 1;
+			LD		R0, M[R7+0x0010];
+			ST		R0, M[R7+0x0600];
+			ADDC	R7, 1;
+			LD		R0, M[R7+0x0010];
+			ST		R0, M[R7+0x0700];
+			ADDC	R7, 1;
+			LD		R0, M[R7+0x0010];
+			ST		R0, M[R7+0x0800];
+			ADDC	R7, 1;
+			SUBC	RF, 1;
+			JMPNZ	@Start;
 
-@OverTestFail	SUB		R0,		R0	 	;
-				ADDC	R0,		1	 	;
-				JMPV	@Start		 	;
-@OverTestPass	SUB		R0,		R0	 	;
-				ADDC	R0,		15	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				JMPV	@ZeroTestFail 	;
+			LD		RF, M[0+0x0000]; % Load Loop Counter to 64 %
+			SUB 	R7, R7;
+			
+@Upper	LD		R0, M[R7+0x0500];
+			ADDV	R0, RE;
+			ST		R0, M[R7+0x0A00];
+			ADDC	R7, 1;
+			SUBC	RF, 1;
+			JMPNZ	@Upper;
 
-@CarTestFail	SUB		R0,		R0	 	;
-				ADDC	R0,		1	 	;
-				JMPC	@Start		 	;
-@CarTestPass	SUB		R0,		R0	 	;
-				NOT		R0;
-				ADDC	R0,		1	 	;
-				JMPC	@NCarTestFail 	;
-
-@NNegTestFail	SUB		R0,		R0	 	;
-				SUBC	R0,		1	 	;
-				JMPNN	@Start		 	;
-@NNegTestPass	SUB		R0,		R0	 	;
-				ADDC	R0,		1	 	;
-				JMPNN	@NOverTestFail	;
-
-@NZeroTestFail	SUB		R0,		R0	 	;
-				JMPNZ	@Start		 	;
-@NZeroTestPass	SUB		R0,		R0	 	;
-				SUBC	R0,		1	 	;
-				JMPNZ	@ReturnTest  	;
-
-@NOverTestFail	SUB		R0,		R0	 	;
-				ADDC	R0,		15	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				ADD		R0,		R0 	 	;
-				JMPNV	@Start		 	;
-@NOverTestPass	SUB		R0,		R0	 	;
-				SUBC	R0,		1	 	;
-				JMPNV	@NZeroTestFail	;
-
-@NCarTestFail	SUB		R0,		R0	 	;
-				NOT		R0;
-				ADDC	R0,		1	 	;
-				JMPNC	@Start		 	;
-@NCarTestPass	SUB		R0,		R0	 	;
-				ADDC	R0,		1	 	;
-				JMPNC	@NNegTestFail	;
-
-@ReturnTest		RET;
-
+			LD		RF, M[0+0x0000]; % Load Loop Counter to 64 %
+			SUB 	R7, R7;
+			
+@Lower	LD		R0, M[R7+0x0600];
+			SUBV	R0, RE;
+			ST		R0, M[R7+0x0B00];
+			ADDC	R7, 1;
+			SUBC	RF, 1;
+			JMPNZ	@Lower;
+			
+			
 .endcode
